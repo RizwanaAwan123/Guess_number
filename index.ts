11487891c978c075206ca296548670e018ec3357
RizwanaAwan123 <awanareeb34@gmail.com>
2024-06-07 19:45:30 +0500

    first commit

---
+++ index.ts
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+import chalk from "chalk";
+
+
+const randomNumber = Math.floor( Math.random() * 10 + 1);
+const answer = await inquirer.prompt([{
+    name : "userguessnumber",
+    type : "number",
+    message:"Guess one number between 1-10",
+
+},
+]);
+
+
+
+if(answer.userguessNumber === randomNumber){
+    console.log(chalk.greenBright.bgCyanBright.bold("Congratulation ! you guessed a right number."));
+
+}else{
+    console.log(chalk.bgGreen.yellowBright.bold("OPPS! You guessed a wrong number"));
+
+}
+
+
+
